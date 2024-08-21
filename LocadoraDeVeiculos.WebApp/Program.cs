9f6a652410aefc28faf99d6abc9d96405422c91c
chayannerech <rechchayanne@gmail.com>
2024-08-21 02:10:16 -0300

    Criado banco de dados

--- LocadoraDeVeiculos.WebApp/Program.cs
+++ LocadoraDeVeiculos.WebApp/Program.cs
@@ -1,4 +1,9 @@
+using LocadoraDeVeiculos.Aplicacao.Servicos;
+using LocadoraDeVeiculos.Dominio.ModuloGrupoDeAutomoveis;
+using LocadoraDeVeiculos.Dominio.ModuloUsuario;
 using LocadoraDeVeiculos.Infra.Orm.Compartilhado;
+using LocadoraDeVeiculos.Infra.Orm.ModuloGrupoDeAutomoveis;
+using Microsoft.AspNetCore.Identity;
 using System.Reflection;
 namespace LocadoraDeVeiculos.WebApp;
 public class Program
@@ -7,14 +12,51 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        builder.Services.AddControllersWithViews();
+
+        #region Injecao de Dependencia de Servico
+
         builder.Services.AddDbContext<LocadoraDeVeiculosDbContext>();
+
+        builder.Services.AddScoped<IRepositorioGrupoDeAutomoveis, RepositorioGrupoDeAutomoveisEmOrm>();
+
+        builder.Services.AddScoped<GrupoDeAutomoveisService>();
+
+        /*  builder.Services.AddIdentity<Usuario, Perfil>()
+                .AddEntityFrameworkStores<LocadoraDeVeiculosDbContext>()
+                .AddDefaultTokenProviders();
+
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 3;
+                options.Password.RequiredUniqueChars = 1;
+            });
+
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.Cookie.Name = "AspNetCore.Cookies";
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                    options.SlidingExpiration = true;
+                });
+
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Usuario/Login";
+                options.AccessDeniedPath = "/Usuario/AcessoNegado";
+            });
+         */
 
         builder.Services.AddAutoMapper(cfg =>
         {
             cfg.AddMaps(Assembly.GetExecutingAssembly());
         });
 
-        builder.Services.AddControllersWithViews();
+        #endregion
 
         var app = builder.Build();
 
